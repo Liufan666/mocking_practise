b99f30c05927f29174757bad52dbe3f416ab613f
kevin_f_liu <1663531467@qq.com>
2020-08-27 19:13:09 +0800

    [mocking] [kevin && david] feature: pass testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName

--- mocking/src/test/java/parking/VipParkingStrategyTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyTest.java
@@ -1,63 +1,90 @@
 package parking;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class VipParkingStrategyTest {
 
-	@Test
-    public void testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
+  @Mock
+  CarDao carDao;
 
-	    /* Exercise 4, Write a test case on VipParkingStrategy.park()
-	    * With using Mockito spy, verify and doReturn */
+  @InjectMocks
+  VipParkingStrategy vipParkingStrategy;
 
-    }
+  @Test
+  public void testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
 
-    @Test
-    public void testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt() {
+    /* Exercise 4, Write a test case on VipParkingStrategy.park()
+     * With using Mockito spy, verify and doReturn */
+      VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+      ParkingLot parkingLot = spy(new ParkingLot("KFC",5));
+      List<ParkingLot> parkingLotList = new ArrayList<>();
+      parkingLotList.add(parkingLot);
+      List<Car> carList = new ArrayList<>();
+      for (int count=0;count<6;count++){
+          carList.add(new Car(""+count));
+      }
+      Car car = new Car("A001");
+      doReturn(carList).when(parkingLot).getParkedCars();
+      doReturn(true).when(vipParkingStrategy).isAllowOverPark(car);
+      Receipt receipt = vipParkingStrategy.park(parkingLotList,car);
 
-        /* Exercise 4, Write a test case on VipParkingStrategy.park()
-         * With using Mockito spy, verify and doReturn */
-    }
+      verify(vipParkingStrategy,times(1)).createReceipt(parkingLot,car);
+      assertEquals("KFC",receipt.getParkingLotName());
+      assertEquals("A001",receipt.getCarName());
 
-    @Test
-    public void testIsAllowOverPark_givenCarNameContainsCharacterAAndIsVipCar_thenReturnTrue(){
+  }
 
-        /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
-         * You may refactor the code, or try to use
-         * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
-         */
-    }
+  @Test
+  public void testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt() {
 
-    @Test
-    public void testIsAllowOverPark_givenCarNameDoesNotContainsCharacterAAndIsVipCar_thenReturnFalse(){
+    /* Exercise 4, Write a test case on VipParkingStrategy.park()
+     * With using Mockito spy, verify and doReturn */
+  }
 
-        /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
-         * You may refactor the code, or try to use
-         * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
-         */
-    }
+  @Test
+  public void testIsAllowOverPark_givenCarNameContainsCharacterAAndIsVipCar_thenReturnTrue() {
 
-    @Test
-    public void testIsAllowOverPark_givenCarNameContainsCharacterAAndIsNotVipCar_thenReturnFalse(){
-        /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
-         * You may refactor the code, or try to use
-         * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
-         */
-    }
+    /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
+     * You may refactor the code, or try to use
+     * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
+     */
+  }
 
-    @Test
-    public void testIsAllowOverPark_givenCarNameDoesNotContainsCharacterAAndIsNotVipCar_thenReturnFalse() {
-        /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
-         * You may refactor the code, or try to use
-         * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
-         */
-    }
+  @Test
+  public void testIsAllowOverPark_givenCarNameDoesNotContainsCharacterAAndIsVipCar_thenReturnFalse() {
 
-    private Car createMockCar(String carName) {
-        Car car = mock(Car.class);
-        when(car.getName()).thenReturn(carName);
-        return car;
-    }
+    /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
+     * You may refactor the code, or try to use
+     * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
+     */
+  }
+
+  @Test
+  public void testIsAllowOverPark_givenCarNameContainsCharacterAAndIsNotVipCar_thenReturnFalse() {
+    /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
+     * You may refactor the code, or try to use
+     * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
+     */
+  }
+
+  @Test
+  public void testIsAllowOverPark_givenCarNameDoesNotContainsCharacterAAndIsNotVipCar_thenReturnFalse() {
+    /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
+     * You may refactor the code, or try to use
+     * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
+     */
+  }
+
+  private Car createMockCar(String carName) {
+    Car car = mock(Car.class);
+    when(car.getName()).thenReturn(carName);
+    return car;
+  }
 }
