271fe3bc84731e4edcc66a6442a91a1635ccb5ab
kevin_f_liu <1663531467@qq.com>
2020-08-27 19:19:56 +0800

    [mocking] [kevin && david] feature: pass testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt

--- mocking/src/test/java/parking/VipParkingStrategyTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyTest.java
@@ -46,6 +46,20 @@ public class VipParkingStrategyTest {
 
     /* Exercise 4, Write a test case on VipParkingStrategy.park()
      * With using Mockito spy, verify and doReturn */
+    VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+    ParkingLot parkingLot = spy(new ParkingLot("KFC",5));
+    List<ParkingLot> parkingLotList = new ArrayList<>();
+    parkingLotList.add(parkingLot);
+    List<Car> carList = new ArrayList<>();
+    for (int count=0;count<6;count++){
+      carList.add(new Car(""+count));
+    }
+    Car car = new Car("A001");
+    doReturn(carList).when(parkingLot).getParkedCars();
+    doReturn(false).when(vipParkingStrategy).isAllowOverPark(car);
+    Receipt receipt = vipParkingStrategy.park(parkingLotList,car);
+
+    verify(vipParkingStrategy,times(1)).createNoSpaceReceipt(car);
   }
 
   @Test
